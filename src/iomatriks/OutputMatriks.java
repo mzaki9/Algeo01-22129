a67c9d960b18b605fe44841dec8389cf6b9d4cc5
MelonSeed9 <130470770+MelonSeed9@users.noreply.github.com>
2023-09-21 19:03:07 +0700

    Added MatrikskeFile

--- src/iomatriks/OutputMatriks.java
+++ src/iomatriks/OutputMatriks.java
@@ -9,4 +9,73 @@ public class OutputMatriks {
             System.out.println();
         }
     }
+
+        
+    private String dir = "test\\result\\";
+    private String path = "";
+    private SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH.mm.ss");
+    
+
+    
+    public void createFile(){
+        try{
+            pathMaker();
+            Date date = new Date();
+            File file = new File(dir + formatter.format(date) + ".txt");
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            path = file.getAbsolutePath();
+        }
+        catch (IOException e){
+            System.out.println("Terjadi error.");
+            JOptionPane.showMessageDialog(null,"Terjadi error. " ,"Error!", JOptionPane.ERROR_MESSAGE); 
+        }
+    }
+        
+    public String matrixToString(double[][] M){
+        String s = "";
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++){
+                if(j == M[i].length){
+                    s += M[i][j];;
+                }
+                else{
+                    s += M[i][j] + " ";
+                }
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+
+    public void pathMaker(){
+        String directory = System.getProperty("user.dir");
+        directory = directory.substring(directory.lastIndexOf("\\")+1);
+        if(directory.equals("bin")){
+            dir = "..\\test\\result\\";
+        }
+        else{
+            dir = "test\\result\\";
+        }
+    }  
+
+    public void MatriksKeTXT(double[][] M){
+        try{
+            createFile();
+            FileWriter wr = new FileWriter(path);
+            String s = matrixToString(M);
+            wr.write(s);
+            wr.close();
+            System.out.println("Sukses menulis file.");
+            JOptionPane.showMessageDialog(null,"Sukses menulis file. " ,"SUKSES", JOptionPane.PLAIN_MESSAGE); 
+        }
+        catch(IOException e){
+            System.out.println("Terjadi error.");
+            JOptionPane.showMessageDialog(null,"Terjadi error. " ,"Error!", JOptionPane.ERROR_MESSAGE); 
+        }
+    }
 }
+    
+
